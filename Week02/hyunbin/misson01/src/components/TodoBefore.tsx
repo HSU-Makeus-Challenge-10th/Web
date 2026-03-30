77ed2fb525541cd736b790345a8635b32ede4e00
hyunbin5921 <guieuna3@gmail.com>
2026-03-30 12:32:16 +0900

    fix: 코드레빗 코멘트 수정

--- Week02/hyunbin/misson01/src/components/TodoBefore.tsx
+++ Week02/hyunbin/misson01/src/components/TodoBefore.tsx
@@ -45,7 +45,7 @@ const TodoBefore = () => {
         <div className='render-container'>
             <div className="render-container__section">
                 <h2 className="render-container__title">할 일</h2>
-                <ul id="todo-List" className="render-container__list">
+                <ul id="active-todo-List" className="render-container__list">
                     {todos.map((todo) => (                    
                         <li key={todo.id}className="render-container__item">
                         <span className="render-container__item-text">{todo.text}</span>
@@ -60,7 +60,7 @@ const TodoBefore = () => {
             </div>
             <div className="render-container__section">
                 <h2 className="render-container__title">완료</h2>
-                <ul id="todo-List" className="render-container__list">
+                <ul id="done-todo-List" className="render-container__list">
                     {doneTodos.map((todo) => (                    
                         <li key={todo.id}className="render-container__item">
                         <span className="render-container__item-text">{todo.text}</span>
