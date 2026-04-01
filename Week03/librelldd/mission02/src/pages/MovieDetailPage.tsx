db97dc719f05aee878491750cc7fed38045ecfc6
librelldd <eldid0407@gmail.com>
2026-04-01 13:53:35 +0900

    mission02

---
+++ Week03/librelldd/mission02/src/pages/MovieDetailPage.tsx
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router-dom';
+
+const MovieDetailPage = () => {
+   
+    const params = useParams();
+
+    console.log(params);
+
+    return <div></div>
+}
+
+export default MovieDetailPage;
