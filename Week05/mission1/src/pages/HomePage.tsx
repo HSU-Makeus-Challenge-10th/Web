0b5004859f1080955839895b0b958310b65e2f3c
KIM JAE WON <0593142@naver.com>
2026-04-29 11:30:21 +0900

    Create Week5 Mission1,2,3

---
+++ Week05/mission1/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-8xl font-bold">돌려돌려 LP</h1>
+    </div>
+  );
+};
+
+export default HomePage;
